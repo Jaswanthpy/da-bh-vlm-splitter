ef152e01c2069f6ff9e38f734fcd0d4eb55c6474
Jaswanthpy <60251024+Jaswanthpy@users.noreply.github.com>
2021-11-02 15:45:29 +0530

    test66-commit

--- vlm-splitter.py
+++ vlm-splitter.py
@@ -33,6 +33,7 @@ def lambda_handler(event, context):
     # bucket = event['Records'][0]['s3']['bucket']['name']
     # key = event['Records'][0]['s3']['object']['key']
     print('text sample')
+    Print('text sample1')
 
     response = pipeline.put_job_success_result(
         jobId=event['CodePipeline.job']['id']
